b77d5665d32ed3a8c6f2869c784a6afcaed6486c
meteora277 <justin.s.diaz@gmail.com>
2021-12-15 17:44:44 -0500

    move createTweetElement into client

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,12 +5,54 @@
  */
 
 $(document).ready(() => {
+  
   const loadTweets = function () {
     $.ajax('/tweets', {method: 'GET'})
     .then(function(data) {
       renderTweets(data)
     })
   };
+  const createTweetElement = function ({ user, content, created_at }) {
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <p><img src="${user.avatars} alt="user avatar""></img>${user.name}</p>
+          <p class='handle'>${user.handle}</p>
+        </header>
+        <section>
+          ${content.text}
+        </section>
+        <footer>
+          ${timeago.format(created_at)}
+          <div>
+            <a href="#">
+              <i class="fas fa-flag"></i>
+            </a>
+            <a href="#">
+              <i class="fas fa-retweet"></i>
+            </a>
+            <a href="#">
+              <i class="fas fa-heart"></i>
+            </a>
+          </div>
+        </footer>
+      </article>
+    `);
+    return $tweet;
+  };
+  
+  //renders data array to tweets container
+  const renderTweets = function(tweets) {
+    
+    let $tweets = $('#tweets-container') 
+    
+    $tweets.empty();
+    
+    for (const tweet of tweets) {
+      $tweets.append(createTweetElement(tweet));
+    }
+    return $tweets
+  }
   loadTweets()
 
   $(".new-tweet > form").submit(function (event) {
@@ -22,3 +64,4 @@ $(document).ready(() => {
     });
   });
 });
+
