7a174fa40913c774e4409b40aacd48be9617ad5c
meteora277 <justin.s.diaz@gmail.com>
2021-12-15 17:30:56 -0500

    wrap file in a document.ready export renderTweet

--- public/scripts/createTweetElement.js
+++ public/scripts/createTweetElement.js
@@ -1,68 +1,75 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+$(document).ready(() => {
+  
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis uwu"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+
+  const createTweetElement = function ({ user, content, created_at }) {
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <p><img src="${user.avatars} alt="user avatar""></img>${user.name}</p>
+          <p class='handle'>${user.handle}</p>
+        </header>
+        <section>
+          ${content.text}
+        </section>
+        <footer>
+          ${timeago.format(created_at)}
+          <div>
+            <a href="#">
+              <i class="fas fa-flag"></i>
+            </a>
+            <a href="#">
+              <i class="fas fa-retweet"></i>
+            </a>
+            <a href="#">
+              <i class="fas fa-heart"></i>
+            </a>
+          </div>
+        </footer>
+      </article>
+    `);
+    return $tweet;
+  };
+  
+  //renders data array to tweets container
+  const renderTweets = function(tweets) {
+    
+    let $tweets = $('#tweets-container') 
+    
+    $tweets.empty();
+    
+    for (const tweet of tweets) {
+      $tweets.append(createTweetElement(tweet));
+    }
+    return $tweets
   }
-]
-
-
-const createTweetElement = function ({ user, content, created_at }) {
-  const $tweet = $(`
-    <article class="tweet">
-      <header>
-        <p><img src="${user.avatars} alt="user avatar""></img>${user.name}</p>
-        <p class='handle'>${user.handle}</p>
-      </header>
-      <section>
-        ${content.text}
-      </section>
-      <footer>
-        ${timeago.format(created_at)}
-        <div>
-          <a href="#">
-            <i class="fas fa-flag"></i>
-          </a>
-          <a href="#">
-            <i class="fas fa-retweet"></i>
-          </a>
-          <a href="#">
-            <i class="fas fa-heart"></i>
-          </a>
-        </div>
-      </footer>
-    </article>`);
-  return $tweet;
-};
-
-const renderTweets = function(tweets) {
-
-  let $tweets = $('#tweets') 
-
-  $tweets.empty();
-
-  for (const tweet of tweets) {
-    $tweets.append(createTweetElement(tweet));
+  
+  module.exports = {
+    renderTweets
   }
-  return $tweets
-}
 
-
-console.log(renderTweets(data))
+})
