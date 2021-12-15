890b3c251a1c5abe86a1760f5fd77d0efec09709
meteora277 <justin.s.diaz@gmail.com>
2021-12-15 17:29:51 -0500

    create logic for loading tweets from db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,21 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(() => {
+  const loadTweets = function () {
+    $.ajax('/tweets', {method: 'GET'})
+    .then(function(data) {
+      renderTweets(data)
+    })
+  };
+  loadTweets()
+
+  $(".new-tweet > form").submit(function (event) {
+    console.log(event);
+    event.preventDefault();
+
+    $.post("/tweets", $(this).serialize(), (response) => {
+      console.log(response, "uwu");
+    });
+  });
+});
